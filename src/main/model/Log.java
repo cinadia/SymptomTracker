5023d439a741841c3c21b478e01334067cefd52b
cinadia <misscindycui@gmail.com>
2022-02-02 13:45:23 -0800

    removed view methods from Logs, intend to put in UI

--- src/main/model/Log.java
+++ src/main/model/Log.java
@@ -4,15 +4,6 @@ import java.util.List;
 
 public interface Log {
 
-    // EFFECTS: show all log entries logged and their index
-    String viewAllLogs();
-
-    // REQUIRES: start and end are valid dates such that:
-    //           start is before end, and
-    //           end is before or equal to today
-    // EFFECTS: show info on the log entries within the given date bounds, empty panel if none
-    String viewLogRange(String start, String end);
-
     // REQUIRES: index >= 0
     // MODIFIES: this
     // EFFECTS: removes the log entry at the given index from this log.
